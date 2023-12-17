f4c11aa1ff7e50e1e7692db7a9634cb80060e206
Vladyslav Kopylets <57573532+deand0n@users.noreply.github.com>
2023-12-17 16:20:29 +0200

    feat: add database types

--- migrations/1702756283950-initial-migration.ts
+++ migrations/1702756283950-initial-migration.ts
@@ -5,8 +5,8 @@ export async function up(db: Kysely<unknown>): Promise<void> {
         .createTable('user')
         .addColumn('id', 'serial', (col) => col.primaryKey())
         .addColumn('first_name', 'varchar', (col) => col.notNull())
-        .addColumn('last_name', 'varchar')
-        .addColumn('gender', 'varchar(50)', (col) => col.notNull())
+        .addColumn('last_name', 'varchar', (col) => col.notNull())
+        .addColumn('description', 'varchar(1000)')
 
         .addColumn('created_at', 'timestamp', (col) => col.defaultTo(sql`now()`).notNull())
         .addColumn('deleted_at', 'timestamp')
@@ -15,6 +15,8 @@ export async function up(db: Kysely<unknown>): Promise<void> {
     await db.schema
         .createTable('service')
         .addColumn('id', 'serial', (col) => col.primaryKey())
+        .addColumn('price', 'int4', (col) => col.notNull())
+        .addColumn('description', 'varchar(1000)')
 
         .addColumn('created_at', 'timestamp', (col) => col.defaultTo(sql`now()`).notNull())
         .addColumn('deleted_at', 'timestamp')
@@ -25,6 +27,7 @@ export async function up(db: Kysely<unknown>): Promise<void> {
     await db.schema
         .createTable('subscriber')
         .addColumn('id', 'serial', (col) => col.primaryKey())
+        .addColumn('description', 'varchar(1000)')
 
         .addColumn('created_at', 'timestamp', (col) => col.defaultTo(sql`now()`).notNull())
         .addColumn('deleted_at', 'timestamp')
--- src/lib/database.types.ts
+++ src/lib/database.types.ts
@@ -1,23 +1,53 @@
-import type { Generated, Insertable, Selectable, Updateable } from 'kysely';
+import type { ColumnType, Generated, Insertable, Selectable, Updateable } from 'kysely';
 
 // https://wanago.io/2023/10/16/api-with-nestjs-129-implementing-soft-deletes-with-sql-and-kysely/
 
 export interface Database {
-    subscription: SubscriptionTable;
+    user: UserTable;
+    service: ServiceTable;
+    subscriber: SubscriberTable;
 }
 
-export interface SubscriptionTable {
-    id: Generated<number>;
-}
-
-export type Subscription = Selectable<SubscriptionTable>;
-export type NewSubscription = Insertable<SubscriptionTable>;
-export type SubscriptionUpdate = Updateable<SubscriptionTable>;
-
-export interface SubscriberTable {
-    id: Generated<number>;
-}
 
 export interface UserTable {
     id: Generated<number>;
+    first_name: string;
+    lase_name: string;
+    description?: string;
+
+    created_at: ColumnType<Date, string | undefined, never>
+    deleted_at: ColumnType<Date | undefined, never, string | undefined>
 }
+
+export type User = Selectable<UserTable>;
+export type NewUser = Insertable<UserTable>;
+export type UserUpdate = Updateable<UserTable>;
+
+
+export interface ServiceTable {
+    id: Generated<number>;
+    price: number;
+    description?: string;
+
+    created_at: ColumnType<Date, string | undefined, never>;
+    deleted_at: ColumnType<Date | undefined, never, string | undefined>;
+
+    owner_id: number;
+}
+
+export type Service = Selectable<ServiceTable>;
+export type NewService = Insertable<ServiceTable>;
+export type ServiceUpdate = Updateable<ServiceTable>;
+
+
+export interface SubscriberTable {
+    id: Generated<number>;
+    description?: string;
+
+    user_id: number;
+    service_id: number;
+}
+
+export type Subscriber = Selectable<SubscriberTable>;
+export type NewSubscriber = Insertable<SubscriberTable>;
+export type SubscriberUpdate = Updateable<SubscriberTable>;
