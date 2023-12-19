16c4163aa3ba050ade6d04edaf223aeb9c23f522
Vladyslav Kopylets <57573532+deand0n@users.noreply.github.com>
2023-12-19 19:11:12 +0200

    feat: add simple theme selector

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,6 +1,37 @@
 <script lang="ts">
-    import { AppBar } from '@skeletonlabs/skeleton';
+    import { browser } from '$app/environment';
+    import { AppBar, popup, type PopupSettings } from '@skeletonlabs/skeleton';
     import { LightSwitch } from '@skeletonlabs/skeleton';
+
+    import { onMount } from 'svelte';
+
+    const popupClick: PopupSettings = {
+        event: 'click',
+        target: 'popupClick',
+        placement: 'top',
+    };
+
+    let body: HTMLBodyElement | null;
+    onMount(() => {
+        body = document.querySelector('body');
+    });
+
+    const themeList = [
+        'hamlindigo',
+        'skeleton',
+        'wintry',
+        'crimson',
+        'gold-nouveau',
+        'modern',
+        'rocket',
+        'sahara',
+        'seafoam',
+        'vintage',
+    ];
+
+    const changeTheme = (theme: string) => {
+        body?.setAttribute('data-theme', theme);
+    };
 </script>
 
 <AppBar>
@@ -10,5 +41,16 @@
     <a href="/"><h1 class="h1">SubscriptionManager</h1></a>
     <svelte:fragment slot="trail">
         <a href="/resources">Resources</a>
+        <button class="btn variant-filled-primary" use:popup={popupClick}>Theme</button>
     </svelte:fragment>
 </AppBar>
+
+<div class="card p-4 variant-filled-primary" data-popup="popupClick">
+    <ul>
+        {#each themeList as theme}
+            <li>
+                <button on:click={() => changeTheme(theme)}>{theme}</button>
+            </li>
+        {/each}
+    </ul>
+</div>
