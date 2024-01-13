b4b078ec3e34fe921ff542d8ca7f9eadf3b24108
Vladyslav Kopylets <57573532+deand0n@users.noreply.github.com>
2024-01-13 16:38:40 +0200

    unscure cookie

--- src/lib/server/lucia.ts
+++ src/lib/server/lucia.ts
@@ -5,7 +5,8 @@ import { pg } from '@lucia-auth/adapter-postgresql';
 import { pool } from '../../../database';
 
 export const auth = lucia({
-    env: dev ? 'DEV' : 'PROD',
+    env: 'DEV',
+    // env: dev ? 'DEV' : 'PROD',
     middleware: sveltekit(),
     adapter: pg(pool, {
         user: 'auth_user',
